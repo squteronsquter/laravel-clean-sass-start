2719389a3cefda37d9f40b1a24164d0b5456c43f
squteronsquter <squter@squter.pl>
2019-05-20 11:36:56 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+console.log('Loading');
+
+window.Vue = require('vue');
+
+Vue.component(
+    'example-component',
+    require('./components/ExampleComponent.vue').default
+);
+
+const app = new Vue({
+    el: '#app'
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/css/app.css">
+
+        <title>Cache Busting</title>
+
+
+
+    </head>
+    <body>
+        <h1>Cache Busting</h1>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
